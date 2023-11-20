015fa9786e970aabb4053064e4c1e223fc3bba4b
Shivaleela <shivaleelakatti13@gmail.com>
2023-11-20 09:35:32 +0530

    adding second player

--- src/bdlabzz1/snakeandladder.java
+++ src/bdlabzz1/snakeandladder.java
@@ -22,10 +22,10 @@ public class snakeandladder {
         int Pos_Player1 = 0;
         System.out.println("welcome player one. Your starting position will be " + Pos_Player1);
      
-        // adding a variable to check the total dice rolls
+     // adding a variable to check the total dice rolls
         int diceRoll_Player1 = 0;
 
-        // we will keep rolling the dice until we get winning position 
+        // we will keep rolling the dice until we get winnig position 
         while(Pos_Player1 != winning_pos){
 
             // now we will call the function of the dice 
@@ -64,8 +64,68 @@ public class snakeandladder {
             // telling the position after every dice roll
             System.out.println("player one rolled the dice and got :" + dice + " and moved to position :" + Pos_Player1);
         }
-        System.out.println("number of times the dice rolled was :" + diceRoll_Player1);
+        // System.out.println("number of times the dice rolled was :" + diceRoll_Player1);
+    
+
+
+        // adding player two
+        int Pos_Player2 = 0;
+        System.out.println("welcome player two. Your starting position will be " + Pos_Player2);
+
+        
+
+        // adding a variable to check the total dice rolls
+        int diceRoll_Player2 = 0;
+
+        // we will keep rolling the dice until we get winnig position 
+        while(Pos_Player2 != winning_pos){
+
+            // now we will call the function of the dice 
+            int dice = Dice();
+            diceRoll_Player2++;
+        
+            // now adding the checks on the game
+            int check = (int) (Math.random() * 100) % 3;
+            switch (check) {
+                case No_Play:
+                    System.out.println("Player2 has got no ladder or snake" + Pos_Player1);
+                    Pos_Player2 = Pos_Player2 + dice;
+                    break;
+
+                case Is_Snake:
+                    System.out.println("player2 has got snake ");
+                    Pos_Player2 = Pos_Player2 - dice;
+                    System.out.println("new position of player 2 will be " + Pos_Player2);
+                    break;
             
+                case Is_Ladder:
+                    System.out.println("player two has got ladder");
+                    Pos_Player2 = Pos_Player2 + dice;
+                    System.out.println("new position of player two will be " + Pos_Player2);
+                    break;
+                
+            }
+            // if the position go below 0 then we will shift it to 0 again
+            if(Pos_Player2<0){
+                Pos_Player2 = 0;
+            }
+            // if the player position go above winning_position then he will stay at same position until he gets exactly winning_pos
+            if (Pos_Player2>winning_pos) {
+                Pos_Player2 = Pos_Player2 - dice;
+            }
+            // telling the position after every dice roll
+            System.out.println("player two rolled the dice and got :" + dice + " and moved to position :" + Pos_Player2);
+        }
+        // telling the no of times each player took to win the game
+        System.out.println("number of times the dice rolled by player one was :" + diceRoll_Player1);
+        System.out.println("number of times the dice rolled by player two was :" + diceRoll_Player2);
+
+        // checking who won the game
+        if(diceRoll_Player1<diceRoll_Player2){
+            System.out.println("player one won the game");
+        }else{
+            System.out.println("player two won the game");
+        }
 	}
 
 }
