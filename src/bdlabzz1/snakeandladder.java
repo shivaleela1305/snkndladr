5ebb561564a891a44a81266abebe62330c559d61
Shivaleela <shivaleelakatti13@gmail.com>
2023-11-20 08:33:15 +0530

    the dice is rolled and the player as moved

--- src/bdlabzz1/snakeandladder.java
+++ src/bdlabzz1/snakeandladder.java
@@ -1,6 +1,12 @@
 package bdlabzz1;
 
 public class snakeandladder {
+	
+	// now we make a function for Dice number
+    static int Dice(){
+        int roll = (int) (Math.random()*100) % 6 + 1;
+        return roll;
+    }
 
 	public static void main(String[] args) {
 		// welcome message
@@ -9,7 +15,11 @@ public class snakeandladder {
         // adding player one
         int Pos_Player1 = 0;
         System.out.println("welcome player one. Your starting position will be " + Pos_Player1);
-
+     
+        // now we will call the function of the dice 
+        int Dice1 = Dice();
+        System.out.print("the dice is rolled and the player position is:" + Dice1);
+            
 	}
 
 }
