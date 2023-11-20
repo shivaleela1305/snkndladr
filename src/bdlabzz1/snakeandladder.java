c510810827f0203bb5fc928c32ca59369842ec77
Shivaleela <shivaleelakatti13@gmail.com>
2023-11-20 08:11:39 +0530

    added a game where game played with single player

---
+++ src/bdlabzz1/snakeandladder.java
@@ -0,0 +1,15 @@
+package bdlabzz1;
+
+public class snakeandladder {
+
+	public static void main(String[] args) {
+		// welcome message
+        System.out.println("welcome to the game of snake and ladders");
+
+        // adding player one
+        int Pos_Player1 = 0;
+        System.out.println("welcome player one. Your starting position will be " + Pos_Player1);
+
+	}
+
+}
