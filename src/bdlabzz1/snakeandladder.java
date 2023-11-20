1fccc6d2aae0a146973e1a18b4c0f626d6a7da88
Shivaleela <shivaleelakatti13@gmail.com>
2023-11-20 09:07:08 +0530

    reporting the times of roll made to win the game

--- src/bdlabzz1/snakeandladder.java
+++ src/bdlabzz1/snakeandladder.java
@@ -22,30 +22,33 @@ public class snakeandladder {
         int Pos_Player1 = 0;
         System.out.println("welcome player one. Your starting position will be " + Pos_Player1);
      
-        // now we will call the function of the dice 
-        int Dice1 = Dice();
-        System.out.print("the dice is rolled and the player position is:" + Dice1);
-        
-     // we will keep rolling the dice until we get winning position 
+        // adding a variable to check the total dice rolls
+        int diceRoll_Player1 = 0;
+
+        // we will keep rolling the dice until we get winning position 
         while(Pos_Player1 != winning_pos){
+
+            // now we will call the function of the dice 
+            int dice = Dice();
+            diceRoll_Player1++;
         
             // now adding the checks on the game
             int check = (int) (Math.random() * 100) % 3;
             switch (check) {
                 case No_Play:
-                    System.out.println("Player1 has got no ladder or snake " + Pos_Player1);
-                    Pos_Player1 = Pos_Player1 + Dice();
+                    System.out.println("Player1 has got no ladder or snake" + Pos_Player1);
+                    Pos_Player1 = Pos_Player1 + dice;
                     break;
 
                 case Is_Snake:
-                    System.out.println("player1 has got snake");
-                    Pos_Player1 = Pos_Player1 - Dice();
+                    System.out.println("player1 has got snake ");
+                    Pos_Player1 = Pos_Player1 - dice;
                     System.out.println("new position of player 1 will be " + Pos_Player1);
                     break;
             
                 case Is_Ladder:
                     System.out.println("player one has got ladder");
-                    Pos_Player1 = Pos_Player1 + Dice();
+                    Pos_Player1 = Pos_Player1 + dice;
                     System.out.println("new position of player one will be " + Pos_Player1);
                     break;
                 
@@ -53,13 +56,15 @@ public class snakeandladder {
             // if the position go below 0 then we will shift it to 0 again
             if(Pos_Player1<0){
                 Pos_Player1 = 0;
-                
-             // if the player position go above winning_position then he will stay at same position until he gets exactly winning_pos
-                if (Pos_Player1>winning_pos) {
-                    Pos_Player1 = Pos_Player1 - Dice();
-                }
-            }           
+            }
+            // if the player position go above winning_position then he will stay at same position until he gets exactly winning_pos
+            if (Pos_Player1>winning_pos) {
+                Pos_Player1 = Pos_Player1 - dice;
+            }
+            // telling the position after every dice roll
+            System.out.println("player one rolled the dice and got :" + dice + " and moved to position :" + Pos_Player1);
         }
+        System.out.println("number of times the dice rolled was :" + diceRoll_Player1);
             
 	}
 
