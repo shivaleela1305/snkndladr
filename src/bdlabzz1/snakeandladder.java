e469da67e6e9eee26e77ca5194af00b2b818cfe2
Shivaleela <shivaleelakatti13@gmail.com>
2023-11-20 08:52:40 +0530

    the dice is rolled untill the player reaches 100

--- src/bdlabzz1/snakeandladder.java
+++ src/bdlabzz1/snakeandladder.java
@@ -13,6 +13,7 @@ public class snakeandladder {
         final int No_Play = 0;
         final int Is_Snake = 1;
         final int Is_Ladder = 2;
+        final int winning_pos = 100;
         
 		// welcome message
         System.out.println("welcome to the game of snake and ladders");
@@ -25,26 +26,34 @@ public class snakeandladder {
         int Dice1 = Dice();
         System.out.print("the dice is rolled and the player position is:" + Dice1);
         
-     // now adding the checks on the game
-        int check = (int) (Math.random() * 100) % 3;
-        switch (check) {
-            case No_Play:
-                System.out.println("Player1 has got no ladder or snake" + Pos_Player1);
-                Pos_Player1 = Pos_Player1 + Dice();
-                break;
-
-            case Is_Snake:
-                System.out.println("player1 has got snake ");
-                Pos_Player1 = Pos_Player1 - Dice();
-                System.out.println("new position of player 1 will be " + Pos_Player1);
-                break;
+     // we will keep rolling the dice until we get winning position 
+        while(Pos_Player1 != winning_pos){
         
-            case Is_Ladder:
-                System.out.println("player one has got ladder");
-                Pos_Player1 = Pos_Player1 + Dice();
-                System.out.println("new position of player one will be " + Pos_Player1);
-                break;
+            // now adding the checks on the game
+            int check = (int) (Math.random() * 100) % 3;
+            switch (check) {
+                case No_Play:
+                    System.out.println("Player1 has got no ladder or snake " + Pos_Player1);
+                    Pos_Player1 = Pos_Player1 + Dice();
+                    break;
+
+                case Is_Snake:
+                    System.out.println("player1 has got snake");
+                    Pos_Player1 = Pos_Player1 - Dice();
+                    System.out.println("new position of player 1 will be " + Pos_Player1);
+                    break;
             
+                case Is_Ladder:
+                    System.out.println("player one has got ladder");
+                    Pos_Player1 = Pos_Player1 + Dice();
+                    System.out.println("new position of player one will be " + Pos_Player1);
+                    break;
+                
+            }
+            // if the position go below 0 then we will shift it to 0 again
+            if(Pos_Player1<0){
+                Pos_Player1 = 0;
+            }           
         }
             
 	}
